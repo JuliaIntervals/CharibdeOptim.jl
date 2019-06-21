5a1cdfaada53c17188f0bc412969f0b831d85707
Yashvardhan Sharma <sharmayashvardhan6@gmail.com>
2019-06-21 19:27:48 +0530

    Added MaxDist.jl and removed Non-parallel version of DE

--- src/IBC.jl
+++ src/IBC.jl
@@ -1,26 +1,29 @@
-function ibc_minimise(g::Function, X::T; ibc_chnl = RemoteChannel(()->Channel{Tuple{T, Float64}}(0)), diffevol_chnl = close(RemoteChannel(()->Channel{Tuple{Vector{Float64}, Float64}}(0))), structure = SortedVector, tol=1e-3 ) where {T}
+function ibc_minimise(g::Function, X::T; ibc_chnl = RemoteChannel(()->Channel{Tuple{T, Float64}}(0)), diffevol_chnl = Nothing, tol=1e-3 ) where {T}
 
     vars = [Variable(Symbol("x",i))() for i in 1:length(X)]
     C = Contractor(vars, g)
     f = X -> g(X...)
 
-    working = structure([(X, inf(f(X)))], x->x[2]) # list of boxes with corresponding lower bound, arranged according to selected structure :
+    working = [X] # list of boxes with corresponding lower bound, arranged according to selected structure :
+    x_best = IntervalBox(Interval.(mid.(X)))
     minimizers = T[]
     global_min = ∞  # upper bound
 
-    num_bisections = 0
+    iter = 0
 
     while !isempty(working)
 
         if isready(ibc_chnl)
             from_diff = take!(ibc_chnl)     # Receiving best individual from ibc_minimise
-            global_min = min(from_diff[2], global_min)
+            if from_diff[2] < global_min
+                (x_best, global_min) = from_diff
+                sort!(working, by = x -> maxdist(x, x_best))
+            end
         end
 
-        (X, X_min) = popfirst!(working)
+        X = pop!(working)
 
-        A = -∞..global_min
-        X = invokelatest(C, A, X)                        # Contracting the box by constraint f(X) < globla_min
+        X = invokelatest(C, -∞..global_min, X)                        # Contracting the box by constraint f(X) < globla_min
         X_min = inf(f(X))
 
         if X_min > global_min    # X_min == inf(f(X))
@@ -32,24 +35,38 @@ function ibc_minimise(g::Function, X::T; ibc_chnl = RemoteChannel(()->Channel{Tu
 
         if m < global_min
             global_min = m
-            if typeof(diffevol_chnl) != Nothing put!(diffevol_chnl, (Vector{Float64}(mid(X)), global_min)) end  # sending best individual to DiffEvaluation
+            x_best = IntervalBox(Interval.(mid.(X)))
+            if diffevol_chnl != Nothing put!(diffevol_chnl, (Vector{Float64}(mid(X)), global_min)) end   # sending best individual to DiffEvaluation
         end
 
-        # Remove all boxes whose lower bound is greater than the current one:
-        filter_elements!(working , (X, global_min) )
+        working = filter(x -> inf(f(x))<global_min, working)
 
         if diam(X) < tol
             push!(minimizers, X)
         else
             X1, X2 = bisect(X)
-            push!( working, (X1, inf(f(X1))) )
-            push!( working, (X2, inf(f(X2))) )
-            num_bisections += 1
+            push!(working, X1, X2)
+            sort!(working, by = x -> maxdist(x, x_best))
         end
+        iter = iter + 1
+    end
 
+    println(iter)
+    if diffevol_chnl != Nothing
+        if isready(diffevol_chnl)
+            take!(diffevol_chnl)
+            put!(diffevol_chnl,(Vector{Float64}(mid(X)), Inf) )
+        else
+            put!(diffevol_chnl,(Vector{Float64}(mid(X)), Inf) )
+        end
+    end
+
+    if isready(ibc_chnl)
+        take!(ibc_chnl)
     end
 
     lower_bound = minimum(inf.(f.(minimizers)))
 
     return Interval(lower_bound,global_min), minimizers
+    #return (global_min, x_best)
 end
