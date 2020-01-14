2fcf04499244ce190d87425d8d2d870c4c61fb6d
Yashvardhan Sharma <sharmayashvardhan6@gmail.com>
2020-01-14 19:26:51 +0530

    Seperating JuMP tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -63,25 +63,6 @@ end
 end
 
 
-@testset "Using JuMP syntax by using only one worker " begin           #for using two workers just dont pass 'workers' arguments as its value is set to 2
-      model = Model(with_optimizer(CharibdeOptim.Optimizer, workers = 1))
-      @variable(model, 1<=x<=2)
-      @variable(model, 1<=y<=2)
-      @NLobjective(model, Min, x^2+y^2)
-      optimize!(model)
-
-      @test JuMP.termination_status(model) == MOI.OPTIMAL
-      @test JuMP.primal_status(model) == MOI.FEASIBLE_POINT
-      @test JuMP.objective_value(model) ≈ 2.0
-      @test JuMP.value(x) ≈ 1.0
-      @test JuMP.value(y) ≈ 1.0
-end
-
-
- # No need to add worker because a worker is already added while running testset "Using Charibde for Constrained Optimsation".
- # Otherwise we have to add a worker by using 'Distributed.addprocs(1)' and load the package on each worker
- # by '@everywhere using CharibdeOptim'.
-
 @testset "Optimising by Charibde (A hybrid approach) using 2 workers" begin
 
       prob = OptimisationProblem(X->((x,y)=X;x^3 + 2y + 5), IntervalBox(2..4, 2..3))
@@ -94,38 +75,6 @@ end
       @test global_min ⊆ 13 .. 13.01
       @test minimisers[1] ⊆ (2.0 .. 2.001) × (3..3.001)
 
-end
-
-
-@testset "Optimising difficult problem using JuMP" begin
-      model = Model(with_optimizer(CharibdeOptim.Optimizer))
-
-      @variable(model, 2 <= x1 <= 3)
-      @variable(model, 3 <= x2 <= 4)
-      @variable(model, 9 <= x3 <= 14)
-      @variable(model, 2 <= x4 <= 3)
-      @variable(model, 3 <= x5 <= 4)
-      @variable(model, 9 <= x6 <= 14)
-      @variable(model, 2 <= x7 <= 3)
-      @variable(model, 3 <= x8 <= 4)
-      @variable(model, 9 <= x9 <= 14)
-
-      @NLobjective(model, Min, x1^2 + x2^2 + x3^4 - x4^7 - 200x5 - x6^5 - x7^9 + x8^5 - 8x9^3)
-
-      optimize!(model)
-
-      @test JuMP.termination_status(model) == MOI.OPTIMAL
-      @test JuMP.primal_status(model) == MOI.FEASIBLE_POINT
-      @test JuMP.objective_value(model) ≈ -575629.0
-      @test JuMP.value(x1) ⊆ 1.99 .. 2.01
-      @test JuMP.value(x2) ⊆ 2.99 .. 3.01
-      @test JuMP.value(x3) ⊆ 8.99 .. 9.01
-      @test JuMP.value(x4) ⊆ 2.99 .. 3.01
-      @test JuMP.value(x5) ⊆ 3.99 .. 4.01
-      @test JuMP.value(x6) ⊆ 13.99 .. 14.01
-      @test JuMP.value(x7) ⊆ 2.99 .. 3.01
-      @test JuMP.value(x8) ⊆ 2.99 .. 3.01
-      @test JuMP.value(x9) ⊆ 13.99 .. 14.01
 end
 
 @testset "Optimising difficult problems using Charibde" begin
